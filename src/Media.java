215c5e68c6dc57b8f40fe9945c8306eeb33d102a
Mac <80626286+MacREpiC@users.noreply.github.com>
2023-11-30 12:17:54 +0100

    Media terminado

---
+++ src/Media.java
@@ -0,0 +1,11 @@
+import org.iesalandalus.programacion.utilidades.Entrada;
+
+public class Media {
+    public static void main(String[] args) {
+        int numero = 0;
+        while(numero < 2) {
+            System.out.print("Dime un número que sea mayor o igual que dos: ");
+            numero = Entrada.entero();
+        }
+    }
+}

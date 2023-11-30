58dac5c42763462cb4356b5da62bbb5a09a03378
Mac <80626286+MacREpiC@users.noreply.github.com>
2023-11-30 12:37:07 +0100

    Media terminado

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+L
+ClasesInteresantesDeJava.iml,0\5\05f7bb3d805c27da91e02a43884cd9c3f74fd34c
+Q
+!.idea/libraries/entrada_1_0_3.xml,1\1\114c4797e7e4e8131f27de601d482077cd953a73
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+y
+Isrc/org/iesalandalus/programacion/clasesinteresantes/arrays/LetraDNI.java,5\3\53657d4ff198e2a2bf810d18ca3d7fca8d536ad4
--- src/org/iesalandalus/programacion/clasesinteresantes/arrays/Media.java
+++ src/org/iesalandalus/programacion/clasesinteresantes/arrays/Media.java
@@ -5,9 +5,39 @@ import org.iesalandalus.programacion.utilidades.Entrada;
 public class Media {
     public static void main(String[] args) {
         int numero = 0;
-        while(numero < 2) {
+        int sumaTotal = 0;
+        int cMayores = 0;
+        int cMenores = 0;
+        int igualesMedia = 0;
+        while(numero < 3) {
             System.out.print("Dime un número que sea mayor o igual que dos: ");
             numero = Entrada.entero();
         }
+        int[] miArray;
+        miArray = new int[numero];
+        for(int i = 0; i < miArray.length; i++){
+            miArray[i] = (int) (Math.random() * 101);
+        }
+        for(int i = 0; i < miArray.length; i++){
+            sumaTotal += miArray[i];
+        }
+        int media = (int) sumaTotal / numero;
+        for(int i = 0; i < miArray.length; i++){
+            if(miArray[i] > media){
+                cMayores++;
+            }
+            if(miArray[i] < media){
+                cMenores++;
+            }
+            if(miArray[i] == media){
+                igualesMedia++;
+            }
+        }
+        for(int i = 0; i < miArray.length; i++){
+            System.out.print(miArray[i] + " ");
+        }
+        System.out.println("Cantidad de número mayores a la media: " + cMayores);
+        System.out.println("Cantidad de número menores a la media: " + cMenores);
+        System.out.println("Cantidad de número iguales a la media: " + igualesMedia);
     }
 }
